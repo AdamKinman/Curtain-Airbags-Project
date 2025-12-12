748edf8493f1ba260a0188dfe2f3e56b6d6ea6fc
AdamKinman <247972726+AdamKinman@users.noreply.github.com>
2025-12-12 11:17:47 +0100

    Bugfix in create_masks.py

--- WindowShapes/create_masks.py
+++ WindowShapes/create_masks.py
@@ -1,24 +1,27 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'sam3'))
+
 import torch
 from PIL import Image
-from sam3.sam3.model_builder import build_sam3_image_model
-from sam3.sam3.model.sam3_image_processor import Sam3Processor
-import os
+from sam3.model_builder import build_sam3_image_model
+from sam3.model.sam3_image_processor import Sam3Processor
 
 
-def createMaskFromImage(imageFileName, processor):
+def createMaskFromImage(imageFileName, processor, PROMPT):
     image = Image.open(imageFileName)
     interference_state = processor.set_image(image)
     output = processor.set_text_prompt(state=interference_state, prompt=PROMPT)
     return output["masks"]
 
-def createMasks(processor):
+def createMasks(processor, IMAGE_FOLDER, OUTPUT_FOLDER, PROMPT):
     if not os.path.exists(OUTPUT_FOLDER):
         os.makedirs(OUTPUT_FOLDER)
 
     for imageFileName in os.listdir(IMAGE_FOLDER):
         if not imageFileName.lower().endswith('.jpg'): continue
         try:
-            mask = createMaskFromImage(os.path.join(IMAGE_FOLDER, imageFileName), processor)
+            mask = createMaskFromImage(os.path.join(IMAGE_FOLDER, imageFileName), processor, PROMPT)
             maskFileName = os.path.join(OUTPUT_FOLDER, f"{imageFileName.split('.')[0]}.pt")
             torch.save(mask, maskFileName)
         except Exception as e:
@@ -28,16 +31,18 @@ def createMasks(processor):
 def run(IMAGE_FOLDER, OUTPUT_FOLDER, PROMPT):
     model = build_sam3_image_model()
     processor = Sam3Processor(model)
-    createMasks(processor)
+    createMasks(processor, IMAGE_FOLDER, OUTPUT_FOLDER, PROMPT)
 
 
 if __name__ == '__main__':
     from config import (HORIZONTAL_ALIGN_IMAGES, WINDOW_MASK_FOLDER, SIDE_MIRROR_MASK_FOLDER)
+    print("Creating window masks...")
     run(
         HORIZONTAL_ALIGN_IMAGES,
         WINDOW_MASK_FOLDER,
         "Side window"
     )
+    print("Creating side mirror masks...")
     run(
         HORIZONTAL_ALIGN_IMAGES,
         SIDE_MIRROR_MASK_FOLDER,
